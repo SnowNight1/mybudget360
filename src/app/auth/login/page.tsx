c70e0d05166c3114e34d8804d6f1b305af713e12
root <root@AVAPC-952102889.>
2025-05-28 17:07:35 +0900

    add other sides & navbar

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/navigation';
 import { Button } from '@/components/ui/button';
 
 export default function LoginPage() {
-  const { data: session, status } = useSession();
+  const {  status } = useSession();
   const router = useRouter();
 
   useEffect(() => {
